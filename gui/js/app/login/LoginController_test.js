0840ccc5472a7e97b735f0a6dd910bc68efcc9c1
Mike Lambert <lambert8@illinois.edu>
2016-04-13 17:37:15 -0500

    End of day - sync

--- gui/js/app/login/LoginController_test.js
+++ gui/js/app/login/LoginController_test.js
@@ -2,16 +2,45 @@
 
 describe('ndslabs module', function() {
 
-  beforeEach(angular.mock.module('ndslabs'));
+	var testUser = 'test';
+	var testPassword = '123456';
+
+  beforeEach(module('ndslabs'),  inject(function(_$value_) {
+	$value = _$value_;
+  }));
+
+  //beforeEach(angular.mock.http.init);
+  //afterEach(angular.mock.http.reset);
+
+  describe('Api', function () {
+    it('should define ApiHost', inject(function ($controller) {
+    	expect($value('ApiHost')).toBeDefined();
+    }));
+    it('should define ApiPort', inject(function ($controller) {
+    	expect($value('ApiPort')).toBe(30001);
+    }));
+  });
 
   describe('LoginController', function () {
-    it('should allow valid login', angular.mock.inject(function ($controller) {
-      var $scope = {};
-      // this is the line that caused me pain
+	beforeEach(inject(function(_$controller_, _$httpBackend_) {
+	  $controller = _$controller_;
+	  $scope = {};
+	  $httpBackend = _$httpBackend_;
+
+	  ApiHost = $value('ApiHost');
+	  ApiPort = $value('ApiPort');
+
+      // Note that this HTTP backend is ngMockE2E's, and will make a real HTTP request
+	  $httpBackend.whenGET('http://' + ApiHost + ':' + ApiPort + '/authenticate').respond(function() {
+	  	return $scope.settings.namespace === testUser && $scope.settings.password === testPassword;
+	  });
+	}));
+
+    it('should allow valid login', inject(function ($controller) {
       $controller('LoginController', { $scope: $scope }); 
 
-      $scope.settings.namespace = 'fake';
-      $scope.settings.password = 'password';
+      $scope.settings.namespace = testUser;
+      $scope.settings.password = testPassword;
 
       $scope.login();
       expect($scope.progressMessage).toBe('Please wait...');
